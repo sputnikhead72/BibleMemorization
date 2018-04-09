6e90fc199c354d2875aabd7ded465bdb8567ae2f
sputnikhead72 <sputnikhead72@CASSINI>
2018-04-08 20:16:50 -0700

    Add project files.

---
+++ Ajax.js
@@ -0,0 +1,57 @@
+﻿
+var Ajax = Ajax || {};
+
+
+
+Ajax.Get = function (url, queryvalues, requestheaders)
+{
+	var request = new XMLHttpRequest ();
+	var querystring = "";
+
+	//	Convert the specified query values into a query string
+	if (queryvalues)
+	{
+		for (var key in queryvalues)
+		{
+			if (querystring.length > 0)
+			{
+				querystring += "&";
+			}
+
+			querystring += encodeURI (key) + "=" + encodeURI (queryvalues [key]);
+		}
+
+		if (querystring.length > 0)
+		{
+			url += "?" + querystring;
+		}
+	}
+
+	//	Initialize the request
+	request.open ("get", url, false);
+
+	//	Add any specified request headers
+	if (requestheaders)
+	{
+		for (var key in requestheaders)
+		{
+			request.setRequestHeader (key, requestheaders [key]);
+		}
+	}
+
+	//	Send the request
+	request.send ();
+
+	//	Wait for response
+	while (request.readyState != 4)
+	{
+	}
+
+	//	Once response is received, check the status, and throw an error if not a 200.
+	if (request.status >= 400)
+	{
+		throw (request.statusText + ": " + request.responseText);
+	}
+
+	return (request.responseText);
+}
